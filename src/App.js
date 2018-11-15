8ef6c7d61144bf37350835f02e6c97ca7b67f651
Erik Golden <erikgolden7@gmail.com>
2018-11-14 20:20:58 -0600

    tests working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
+import Modal from './Modal';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      isChecked: false
+    };
+  }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  onChange = () => this.setState({ isChecked: !this.state.isChecked });
+
   render() {
-    return <div className="App">Testing Application</div>;
+    const { showModal } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleModal}>Toggle Menu</button>
+        {showModal ? (
+          <Modal>
+            <h1>Would you like to adopt a puppy?</h1>
+            <div className="buttons">
+              <button onClick={this.toggleModal}>Yes</button>
+              <button onClick={this.toggleModal}>No</button>
+            </div>
+          </Modal>
+        ) : null}
+
+        <label>
+          <input type="checkbox" checked={this.state.isChecked} onChange={this.onChange} />
+          {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+        </label>
+      </div>
+    );
   }
 }
 
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as TestUtils from 'react-dom/test-utils';
+import { create } from 'react-test-renderer';
 import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('snapshot', () => {
+  const c = create(<App />);
+  expect(c.toJSON()).toMatchSnapshot();
+});
+
+test('shows modal when toggleModal is called', () => {
+  const component = create(<App />);
+  const instance = component.getInstance();
+
+  expect(instance.state.showModal).toBe(false);
+  instance.toggleModal();
+  expect(instance.state.showModal).toBe(true);
+});
+
+it('CheckboxWithLabel changes the text after click', () => {
+  // Render a checkbox with label in the document
+  const checkbox = TestUtils.renderIntoDocument(<App labelOn="On" labelOff="Off" />);
+
+  const checkboxNode = ReactDOM.findDOMNode(checkbox);
+
+  // Verify that it's Off by default
+  expect(checkboxNode.textContent).toEqual('Toggle MenuOff');
+
+  // Simulate a click and verify that it is now On
+  TestUtils.Simulate.change(TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input'));
+  expect(checkboxNode.textContent).toEqual('Toggle MenuOn');
 });
